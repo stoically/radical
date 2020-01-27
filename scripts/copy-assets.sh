e848bba0935807a38f38d87e4765154ecc4d055d
stoically <stoically@protonmail.com>
2020-01-27 18:44:49 +0100

    fix(scripts): include jsoneditor.js

--- scripts/copy-assets.sh
+++ scripts/copy-assets.sh
@@ -8,7 +8,7 @@ cp src/manifest.firefox.json build/firefox/manifest.json
 cp -r src/vector-icons build/firefox
 cp LICENSE build/firefox
 mkdir -p build/firefox/vendor/jsoneditor
-cp node_modules/jsoneditor/dist/jsoneditor.min.css build/firefox/vendor/jsoneditor
+cp node_modules/jsoneditor/dist/jsoneditor.min.* build/firefox/vendor/jsoneditor
 cp -r node_modules/jsoneditor/dist/img build/firefox/vendor/jsoneditor
 
 # chrome
