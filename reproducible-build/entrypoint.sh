89c79151f9908948cb9c85774ecada1d5b9f75a2
stoically <stoically@protonmail.com>
2020-01-30 09:34:00 +0100

    feat: reproducible builds (#17)

---
+++ reproducible-build/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ex
+
+export FIREFOX_EXTENSION_URL="https://addons.mozilla.org/firefox/downloads/file/3493665/riot-latest-fx.xpi"
+export CHROME_EXTENSION_URL="https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3Dlgpdpggoahhnlmaiompkgghldllldcjh%26installsource%3Dondemand%26uc"
+
+if [[ -n $1 ]]; then
+  exec "$@"
+fi
+
+build () {
+  echo "Building $1"
+  rm -rf $1
+  mkdir $1
+  cd $1
+
+  wget $2 -O $1-extension.zip
+  unzip $1-extension.zip -d $1-extension || true
+  EXTENSION_VERSION="v$(jq -r '.version' $1-extension/manifest.json)"
+  echo "$1 extension Version: $EXTENSION_VERSION"
+
+  git clone https://github.com/stoically/riot-webext
+  cd riot-webext
+  $2 | xargs git checkout
+  npm install
+  npm run build
+
+  echo "Diffing $1"
+  diff -u -r build/$1 ../$1-extension || true
+
+  cd ../..
+}
+
+build "firefox" $FIREFOX_EXTENSION_URL
+build "chrome" $CHROME_EXTENSION_URL
